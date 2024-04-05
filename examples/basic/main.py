9c0b668597566867897e2f00279f465bd884242d
Eric Chen <kych@berkeley.edu>
2024-04-05 06:19:15 +0000

    fine tune example

--- examples/basic/main.py
+++ examples/basic/main.py
@@ -8,19 +8,18 @@ dataset = fog_rtx.dataset.Dataset(
     db_connector=fog_rtx.database.DatabaseConnector("/tmp/rtx.db"),
 )
 
-for i in range(10):
+for i in range(1,10):
     # create a new episode / trajectory
     episode = dataset.new_episode(
         metadata={
-            "collector_name": "John Doe",
+            "collector_name": f"User #{i}",
             "description": f"description #{i}",
         }
     )
-
     # populate the episode with FeatureTypes
-    for i in range(10):
-        episode.add(feature="arm_view", value=f"image{i}.jpg")
-        episode.add(feature="camera_pose", value=f"pose{i}")
+    for j in range(1, 4):
+        episode.add(feature="feature_1", value=f"episode{i}_step{j}_feature_1")
+        episode.add(feature="feature_2", value=f"episode{i}_pose{j}_feature_2")
     episode.close()
 
 # mark the current state as terminal state
