8f2d2be00deecc702970ef292bfb97e7d66b6e65
Eric Chen <kych@berkeley.edu>
2024-04-05 19:19:17 +0000

    Update dataset loading in main.py and add import statement in test_base.py

--- examples/pytorch/main.py
+++ examples/pytorch/main.py
@@ -7,13 +7,22 @@ dataset = fog_rtx.dataset.Dataset(
     path="/tmp",
 )
 
-# dataset.load_rtx_episodes(
-#     name="berkeley_autolab_ur5",
-#     split="train[:1]",
-# )
+dataset.load_rtx_episodes(
+    name="berkeley_autolab_ur5",
+    split="train[:2]",
+    additional_metadata={"collector": "User 1"},
+)
+
+dataset.load_rtx_episodes(
+    name="berkeley_autolab_ur5",
+    split="train[3:5]",
+    additional_metadata={"collector": "User 2"},
+)
 
 
-pytorch_ds = dataset.pytorch_dataset_builder()
+pytorch_ds = dataset.pytorch_dataset_builder(
+    metadata=dataset.get_metadata_as_pandas_df()
+)
 
 # get samples from the dataset
 for data in torch.utils.data.DataLoader(
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,1 +1,3 @@
 
+import fog_rtx
+
