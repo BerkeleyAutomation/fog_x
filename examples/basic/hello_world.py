c7c9284cd4326ec58020980111a86c2f66eb8745
Kaiyuan Eric Chen <kych@berkeley.edu>
2024-08-21 01:35:07 -0700

    Refactor Trajectory class to remove commented code and improve code readability

--- examples/basic/hello_world.py
+++ examples/basic/hello_world.py
@@ -4,43 +4,6 @@ import time
 
 path = "/tmp/output.vla"
 
-
-# import av
-
-# def remux_mkv(input_filename, output_filename):
-#     # Open the input file using PyAV
-#     input_container = av.open(input_filename, format = "matroska")
-
-#     # Create an output container for the new file
-#     output_container = av.open(output_filename, mode='w', format='matroska')
-
-#     # Loop through all streams in the input file and add them to the output file
-#     for stream in input_container.streams:
-#         output_container.add_stream(stream.codec_context.codec.name)
-#         print(stream.codec_context.codec.name)
-
-#     # Read packets from the input file and write them to the output file
-#     for packet in input_container.demux():
-#         if packet.dts is None:
-#             print("Skipping packet with no dts")
-#             continue
-#         stream = output_container.streams[packet.stream.index]
-#         print(packet.stream.metadata, packet)
-#         packet.stream = stream
-#         output_container.mux(packet)
-
-#     # Close both containers
-#     output_container.close()
-#     input_container.close()
-
-# input_filename = "/home/kych/datasets/rtx/mkv_convert/output_0.mkv"#"/tmp/output.vla"
-# input_filename = "/tmp/output.vla"
-# output_filename = "/tmp/remuxed.mkv"
-
-# remux_mkv(input_filename, output_filename)
-
-# exit(0)
-
 # remove the existing file
 import os
 os.system(f"rm -rf {path}")
