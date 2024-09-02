e6185717869dc15897efab291194215e7db3ede6
Kaiyuan Eric Chen <kych@berkeley.edu>
2024-09-02 04:35:41 -0700

    Refactor LeRobotLoader to load one episode at a time for improved performance and memory efficiency

--- fog_x/loader/lerobot.py
+++ fog_x/loader/lerobot.py
@@ -8,13 +8,7 @@ class LeRobotLoader(BaseLoader):
         super(LeRobotLoader, self).__init__(path)
         self.batch_size = batch_size
         self.dataset = LeRobotDataset(root="/mnt/data/fog_x/hf/", repo_id=dataset_name, delta_timestamps=delta_timestamps)
-        self.dataloader = torch.utils.data.DataLoader(
-            self.dataset,
-            batch_size=1,  # Load one episode at a time
-            shuffle=True,
-        )
-        self.iterator = iter(self.dataloader)
-        self.current_episode_index = None
+        self.episode_index = 0
 
     def __len__(self):
         return len(self.dataset)
@@ -30,25 +24,23 @@ class LeRobotLoader(BaseLoader):
             return {k: np.array(v) for k, v in frame.items()}
         for _ in range(self.batch_size):
             episode = []
+            # repeat
+            if self.episode_index >= len(self.dataset):
+                self.episode_index = 0
+                
             for attempt in range(max_retries):
                 try:
-                    batch = next(self.iterator)
-                    episode_index = batch["episode_index"][0].item()
-
-                    from_idx = self.dataset.episode_data_index["from"][episode_index].item()
-                    to_idx = self.dataset.episode_data_index["to"][episode_index].item()
+                    from_idx = self.dataset.episode_data_index["from"][self.episode_index].item()
+                    to_idx = self.dataset.episode_data_index["to"][self.episode_index].item()
                     frames = [_frame_to_numpy(self.dataset[idx]) for idx in range(from_idx, to_idx)]
-
                     episode.extend(frames)
+                    self.episode_index += 1
                     break
-                except StopIteration:
-                    self.iterator = iter(self.dataloader)
-                    if attempt == max_retries - 1:
-                        raise StopIteration
                 except Exception as e:
-                    self.iterator = iter(self.dataloader)
                     if attempt == max_retries - 1:
                         raise e
+                    self.episode_index += 1
+
 
             batch_of_episodes.append((episode))
             
